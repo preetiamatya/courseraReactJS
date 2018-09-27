b481c8442500a9b4cf2f93efccb6f3e7d5231c1c
preetiamatya <preetiamatya@gmail.com>
2018-09-27 13:42:18 +0200

    format codes

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label} from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 class Header extends Component {
   constructor(props) {
@@ -11,101 +11,102 @@ class Header extends Component {
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
- }
-    toggleNav() {
-      this.setState ({
-        isNavOpen : !this.state.isNavOpen
-      });
   }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
+  
   toggleModal() {
-    this.setState ({
-      isModalOpen : !this.state.isModalOpen
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
     });
   }
   handleLogin(event) {
-        this.toggleModal();
-        alert("Username: " + this.username.value + " Password: " + this.password.value
-            + " Remember: " + this.remember.checked);
-        event.preventDefault();
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+      + " Remember: " + this.remember.checked);
+    event.preventDefault();
 
-    }
+  }
   render() {
     return (
-    <React.Fragment>
-    {/*react fragment*/}
-    {/*adding React elements directly to the DOM*/}
-    <Navbar dark expand = "md">
-    <NavbarToggler onClick = {this.toggleNav}/>
-      <div className="container">
-        <NavbarBrand className="mr-auto" href="/">
-          <img src = "assets/images/logo.png" height="30" width = "41" alt = "Ristorante Con Fusion"/>
-        </NavbarBrand>
-        <Collapse isOpen = {this.state.isNavOpen} navbar>
-        <Nav navbar>
-        <NavItem>
-          <NavLink className="nav-link"  to='/home'>
-            <span className="fa fa-home fa-lg"></span> Home
+      <React.Fragment>
+        {/*react fragment*/}
+        {/*adding React elements directly to the DOM*/}
+        <Navbar dark expand="md">
+          <NavbarToggler onClick={this.toggleNav} />
+          <div className="container">
+            <NavbarBrand className="mr-auto" href="/">
+              <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion" />
+            </NavbarBrand>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to='/home'>
+                    <span className="fa fa-home fa-lg"></span> Home
           </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link" to='/aboutus'>
-            <span className="fa fa-info fa-lg"></span> About Us
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/aboutus'>
+                    <span className="fa fa-info fa-lg"></span> About Us
           </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink className="nav-link"  to='/menu'>
-            <span className="fa fa-list fa-lg"></span> Menu
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/menu'>
+                    <span className="fa fa-list fa-lg"></span> Menu
           </NavLink>
-        </NavItem>
-        <NavItem>
-            <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us
             </NavLink>
-        </NavItem>
-        </Nav>
-        <Nav className = "ml-auto" navbar>
-          <NavItem>
-          <Button outline onClick ={this.toggleModal}>
-          <span className = "fa fa-sign-in fa-lg"></span>Login
+                </NavItem>
+              </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span>Login
           </Button>
-          </NavItem>
-        </Nav>
-        </Collapse>
-      </div>
-    </Navbar>
-    <Jumbotron>
-      <div className = "container">
-        <div className = "row row-header">
-          <div className = "col-12 col-sm-6">
-          <h1>Ristorante con Fusion</h1>
-          <p>We take inspiration from the Worlds best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
-        </div>
-      </div>
-    </Jumbotron>
-    <Modal isOpen = {this.state.isModalOpen} toggle = {this.toggleModal}>
-      <ModalHeader toggle = {this.toggleModal}>Login</ModalHeader>
-      <ModalBody>
-      <Form onSubmit = {this.handleLogin}>
-        <FormGroup>
-          <Label htmlFor = "username"> Username</Label>
-          <Input type="text" id="username" name = "username" innerRef = {(input) => this.username = input}/>
-        </FormGroup>
-        <FormGroup>
-          <Label htmlFor = "password"> Password</Label>
-          <Input type="password" id="password" name = "password" innerRef = {(input) => this.password = input}  />
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" name="remember" innerRef = {(input) => this.remember = input}/>
-            Remember me
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Ristorante con Fusion</h1>
+                <p>We take inspiration from the Worlds best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username"> Username</Label>
+                <Input type="text" id="username" name="username" innerRef={(input) => this.username = input} />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password"> Password</Label>
+                <Input type="password" id="password" name="password" innerRef={(input) => this.password = input} />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input} />
+                  Remember me
           </Label>
-        </FormGroup>
-        <Button type="submit" className="bg-primary" color="primary">Login </Button>
-      </Form>
-      </ModalBody>
-    </Modal>
-    </React.Fragment>
-  );
+              </FormGroup>
+              <Button type="submit" className="bg-primary" color="primary">Login </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </React.Fragment>
+    );
   }
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,32 +23,32 @@ class Main extends Component {
   render() {
     const HomePage = () => {
       return (
-        <Home dish = {this.props.dishes.filter((dish)=>dish.featured)[0]}
-        promotion = {this.props.promotions.filter((promo)=>promo.featured)[0]}
-        leader = {this.props.leaders.filter((leader)=>leader.featured)[0]}
+        <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.props.leaders.filter((leader) => leader.featured)[0]}
         />
       );
     }
-    const DishWithId = ({match}) => {
+    const DishWithId = ({ match }) => {
       return (
-        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
       );
     }
     return (
       <div>
-      <Header/>
-      <Switch>
-        <Route path = "/home" component = {HomePage}/>
-         {/*pass the props with route as function component */}
-         <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-         <Route path = '/menu/:dishId' component = {DishWithId} />
-         <Route exact path='/contactus' component={Contact} />
-         <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
-         {/*default path*/}
-         <Redirect to="/home" />
-      </Switch>
-      <Footer/>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          {/*pass the props with route as function component */}
+          <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+          {/*default path*/}
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
